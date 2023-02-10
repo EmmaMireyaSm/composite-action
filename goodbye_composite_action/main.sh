aa24d724e5a20d096ef4ede2a63c1650886ae17f
EmmaMireyaSm <emireya.sm@gmail.com>
2023-02-10 17:46:34 -0600

    feat: Add Dockerfiles

--- goodbye_composite_action/main.sh
+++ goodbye_composite_action/main.sh
@@ -1,1 +1,18 @@
-echo "Goodbye"
+set_output() {
+  local -r key="$1";
+  local -r value="$2";
+  echo "name=$key::$value"
+  echo "::set-output name=$key::$value"
+}
+
+set -o pipefail
+
+PERSON="$1"
+PATH="$2"
+
+
+echo "Goodbye $PERSON"
+RANDOM_NUMBER=$(echo $RANDOM)
+
+set_output "random-number" "$RANDOM_NUMBER"
+set_output "github-path" "$PATH"
