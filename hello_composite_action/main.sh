df7895483a20f02ce2d91249a711101296473b6a
EmmaMireyaSm <emireya.sm@gmail.com>
2023-02-10 18:36:22 -0600

    fix: remove path

--- hello_composite_action/main.sh
+++ hello_composite_action/main.sh
@@ -8,13 +8,8 @@ set_output() {
 set -o pipefail
 
 PERSON="$1"
-EVENT_PATH="$2"
-PATH="$3"
-
 
 echo "Hello $PERSON"
 RANDOM_NUMBER=$(echo $RANDOM)
 
 set_output "random-number" "$RANDOM_NUMBER"
-set_output "event-path" "$EVENT_PATH"
-set_output "path" "$PATH"
